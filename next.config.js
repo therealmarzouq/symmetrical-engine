f420693685a722384bd693c9f7c55526c85ae9c2
Marzouq <marzouq.mohammed@cicod.com>
2022-04-24 11:15:41 +0100

    +

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ module.exports = withContentlayer()({
   images: {
     domains: [
       'i.scdn.co', // Spotify Album Art
-      'pbs.twimg.com' // Twitter Profile Picture
+      'pbs.twimg.com', // Twitter Profile Picture
+      'vitals.vercel-insights.com'
     ]
   },
   webpack: (config, { dev, isServer }) => {
